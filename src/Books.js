fd958de5c5dcad295b2226d64e360e6db8a99181
simon.dudley <simon.dudley@autotrader.co.uk>
2018-11-07 16:18:51 +0000

    Initial commit

---
+++ src/Books.js
@@ -0,0 +1,19 @@
+import React, {Suspense} from "react";
+import Img from "./Img";
+
+// const Img = React.lazy(() => import('./Img'));
+
+export default function Books({ content }) {
+
+    return (
+        <React.ConcurrentMode>
+            <React.Fragment>
+                <h1>Book Gallery</h1>
+                <p>{content.name}</p>
+                <Suspense maxDuration={100} fallback={<div>Loading Image...</div>}>
+                    <Img src={content.image} alt={content.name} />
+                </Suspense>
+            </React.Fragment>
+        </React.ConcurrentMode>
+    );
+}
---
+++ src/Img.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { unstable_createResource } from 'react-cache'
+
+const ImageResource = unstable_createResource(
+    source =>
+        new Promise(resolve => {
+            const img = new Image();
+            img.src = source;
+            img.onload = resolve;
+        })
+)
+
+export const Img = ({src, alt}) => {
+    ImageResource.read(src);
+    return <img src={src} alt={alt} />
+}
+
+export default Img
